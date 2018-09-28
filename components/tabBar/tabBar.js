0463735834a75759f6f69a2a51ed8258482907b6
weikx <weikexin666@gmail.com>
2018-09-29 00:31:09 +0800

    修改 tabBar 组件

--- components/tabBar/tabBar.js
+++ components/tabBar/tabBar.js
@@ -11,13 +11,19 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    type: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    switchTab (event) {
+      let index = event.currentTarget.dataset.index
+      this.triggerEvent('switchTab', index)
+      this.setData({
+        type: index || 0
+      })
+    }
   }
 })
