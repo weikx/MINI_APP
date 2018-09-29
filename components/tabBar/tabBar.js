abd7ce2afb249fc3f3fcbb965e5231497f1c9c8c
weikx <weikexin666@gmail.com>
2018-09-29 18:27:49 +0800

    修改切换逻辑

--- components/tabBar/tabBar.js
+++ components/tabBar/tabBar.js
@@ -1,28 +1,20 @@
 // components/tabBar/tabBar.js
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
 
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
     type: 0
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
     switchTab (event) {
       let index = event.currentTarget.dataset.index
       this.triggerEvent('switchTab', index)
       this.setData({
-        type: index || 0
+        type: index || 0,
+        nodeRotate: index == 2 ? 360 : 0
       })
     }
   }
